bdb7cb471612072071b890652a545270a1ba4cbf
Richard L. Lao <richard.layhout.lao@gmail.com>
2020-09-01 14:58:50 -0400

    Updates

---
+++ precache-manifest.808bed47385deb0ea1b213d3e9c7fcc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33287bdeaa5758340aed26f5953602a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c6158d38392eda77e78",
+    "url": "/static/css/main.04eb0484.chunk.css"
+  },
+  {
+    "revision": "0ac27ccecaca5553eea2",
+    "url": "/static/js/2.640f7b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.640f7b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6158d38392eda77e78",
+    "url": "/static/js/main.ea7c0cf5.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
